ed0df7656754efdd0fc98db6f593f793b72dde7a
Gerardo Gallegos <puxerop@gmail.com>
2019-06-21 09:30:05 -0500

    It's removed swig as template engine and I add mongoDB connection

--- app/connections/DB.js
+++ app/connections/DB.js
@@ -1,5 +1,3 @@
-//  Conexion a base de datos
-
 const mongoose = require('mongoose')
 
 mongoose
@@ -8,13 +6,3 @@ mongoose
     console.log('ERROR MONGO-DB::', error)
     throw error
   })
-
-// module.exports = function (DB_NAME, mongoURI) {
-//   console.log('DB____' + DB_NAME)
-//   return mongoose.createConnection('mongodb://localhost/' + DB_NAME, function (err) {
-//     if (err) {
-//       console.log(err)
-//       throw (err)
-//     }
-//   })
-// }
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 
 'use strict'
 
-// Config env variables .env
+// Config env var .env
 require('dotenv').config()
+
+// Connection MongoDB
+require('./app/connections/DB')
 
 const express = require('express')
 const path = require('path')
@@ -12,7 +15,6 @@ const io = require('socket.io')(http)
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const methodOverride = require('method-override')
-const swig = require('swig')
 const multiparty = require('connect-multiparty')
 const API = require('./app/controllers/api')
 
@@ -21,16 +23,9 @@ const multipartyMiddleware = multiparty()
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json({ limit: '5mb' }))
-// Handle request DELETE-PUT
 app.use(methodOverride())
-// Parse multiparte
 app.use(multipartyMiddleware)
-// Enable estaticos
-app.use(express.static('./public'))
-// Config swig
-app.engine('html', swig.renderFile)
-app.set('view engine', 'html')
-app.set('views', path.join(__dirname, '/app/views'))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(API)
 
 // Sockets management
