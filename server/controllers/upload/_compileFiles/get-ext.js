103002307d2758528f39056d8b73e8dabe6f90ea
Gerardo Gallegos <puxerop@gmail.com>
2019-06-26 14:21:31 -0500

    Was deleted deprecated upload controller

--- server/controllers/upload/_compileFiles/get-ext.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-function getExt (name) {
-  const cleanName = name.substr(-4)
-  return cleanName.substring(cleanName.search(/\./g) + 1, cleanName.length)
-}
-
-module.exports = getExt
