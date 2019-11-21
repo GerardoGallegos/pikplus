51c97b8684fd8417d42a5242f5104736a29c7a6f
Gerardo Gallegos <puxerop@gmail.com>
2019-11-20 23:38:46 -0600

    Update design model

--- app/models/design.js
+++ app/models/design.js
@@ -6,7 +6,33 @@ const DesignSchema = new Schema({
     type: String,
     default: shortid.generate
   },
-  author: { type: String, ref: 'User', required: [true, 'El Author es requerido'] }
+  author: { type: String, ref: 'User', required: [true, 'El Author es requerido'] },
+  description: String,
+  colorsRGB: [
+    {
+      r: Number,
+      g: Number,
+      b: Number,
+      p: Number
+    }
+  ],
+  colorsLAB: [
+    {
+      L: Number,
+      A: Number,
+      B: Number,
+      p: Number
+    }
+  ],
+  source: {
+    s30: String,
+    s100: String,
+    s150: String,
+    s200: String,
+    s300: String,
+    s1000: String
+  }
+
 }, { timestamps: true })
 
 module.exports = model('Design', DesignSchema)
